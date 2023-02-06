5c31b3b80162cc4cf2f23e1d66284473f0112be1
Mary Mackemzie S. Salazar <mackemzie0420@gmail.com>
2023-02-06 18:48:33 +0800

    Add game.js for config

---
+++ game.js
@@ -0,0 +1,22 @@
+var gameSettings = {
+    playerSpeed: 200,
+    maxPowerups: 4,
+    powerUpVel: 100,
+  }
+
+var config = {
+    width: 256,
+    height: 272,
+    backgroundColor: 0x000000,
+    scene: [Scene1, Scene2],
+    pixelArt: true,
+    physics: {
+        default: "arcade",
+        arcade:{
+            debug: false
+        }
+      }
+}
+
+var game = new Phaser.Game(config);
+
